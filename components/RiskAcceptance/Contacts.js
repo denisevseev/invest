3f2571205647e0611a71ef50920a58c419e0b809
denisevseev <08038282@mail.ru>
2024-07-18 01:04:31 +0300

    if  not press accept i show modal

--- components/RiskAcceptance/Contacts.js
+++ components/RiskAcceptance/Contacts.js
@@ -1,4 +1,4 @@
-import {Box, Typography} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export default function Contacts(props) {
@@ -8,43 +8,59 @@ export default function Contacts(props) {
                 display: 'flex',
                 justifyContent: 'space-between',
                 mt: 4,
-                mb: 2,
-                p: 2,
-                borderRadius: '4px',
             }}
         >
             <Box sx={{ textAlign: 'center', bgcolor: '#d3d3d3', p: 2, borderRadius: '4px', flex: 1, m: 1 }}>
-                <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+                <Typography variant="body2" sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>
                     2102-58 Keefer Place
                 </Typography>
-                <Typography variant="body2">Vancouver, BC V6B 0B6</Typography>
-                <Typography variant="body2">Canada</Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    Vancouver, BC V6B 0B6
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    Canada
+                </Typography>
             </Box>
             <Box sx={{ textAlign: 'center', bgcolor: '#d3d3d3', p: 2, borderRadius: '4px', flex: 1, m: 1 }}>
-                <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+                <Typography variant="body2" sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>
                     Phone: +1 778-9002-237
                 </Typography>
-                <Typography variant="body2">Fax: +1 205-7402-846</Typography>
-                <Typography variant="body2">Email: support@ramford-inc.com</Typography>
-                <Typography variant="body2">Internet: www.ramford-inc.com</Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    Fax: +1 205-7402-846
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    Email: support@ramford-inc.com
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    Internet: www.ramford-inc.com
+                </Typography>
             </Box>
             <Box sx={{ textAlign: 'center', bgcolor: '#d3d3d3', p: 2, borderRadius: '4px', flex: 1, m: 1 }}>
-                <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+                <Typography variant="body2" sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>
                     TransferWise Europe SA
                 </Typography>
-                <Typography variant="body2">SWIFT/BIC: TRWIBEB1XXX</Typography>
-                <Typography variant="body2">IBAN: BE79 9671 7187 1333</Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    SWIFT/BIC: TRWIBEB1XXX
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    IBAN: BE79 9671 7187 1333
+                </Typography>
             </Box>
             <Box sx={{ textAlign: 'center', bgcolor: '#d3d3d3', p: 2, borderRadius: '4px', flex: 1, m: 1 }}>
-                <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+                <Typography variant="body2" sx={{ fontWeight: 'bold', fontSize: '1.3rem' }}>
                     TD Canada Trust
                 </Typography>
-                <Typography variant="body2">SWIFT/BIC: TDOMCATTOR</Typography>
-                <Typography variant="body2">BLZ/Wire: 53784-004</Typography>
-                <Typography variant="body2">Konto-Nr: 5017003</Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    SWIFT/BIC: TDOMCATTOR
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    BLZ/Wire: 53784-004
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: '1.3rem' }}>
+                    Konto-Nr: 5017003
+                </Typography>
             </Box>
         </Box>
     )
 
 }
-
--- components/RiskAcceptance/RiskAcceptanceModal.js
+++ components/RiskAcceptance/RiskAcceptanceModal.js
@@ -1,21 +1,29 @@
+
 import React, { useState, useEffect } from 'react';
 import { Modal, Box, Typography, Button, Checkbox, FormControlLabel } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 import Contacts from "./Contacts";
+
+const theme = createTheme({
+    typography: {
+        fontSize: 16, //  Базовый размер шрифта
+    },
+});
 
 const RiskAcceptanceModal = () => {
     const [open, setOpen] = useState(false);
     const [accepted, setAccepted] = useState(false);
+    const isModalShown = localStorage.getItem('riskModalShown');
 
     useEffect(() => {
-        const timer = setTimeout(() => {
+        if (!isModalShown) {
             setOpen(true);
-        }, 3000);
-
-        return () => clearTimeout(timer);
+        }
     }, []);
 
     const handleClose = () => {
         setOpen(false);
+        localStorage.setItem('riskModalShown', 'true');
     };
 
     const handleAcceptChange = (event) => {
@@ -116,38 +124,38 @@ const RiskAcceptanceModal = () => {
 
                 </Box>
 
-                <Typography variant="h5" gutterBottom>
+                <Typography variant="h5" gutterBottom sx={{ fontSize: '1.4rem' }}>
                     CHANCEN UND RISIKEN BEI EINER KAPITALANLAGE IN VORBÖRSLICHE AKTIEN
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     (Zusammenfassung der wichtigsten Risiken)
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Die Kapitalanlage in Aktien ist mit Chancen – aber natürlich auch mit Risiken verbunden. Es sollten daher bei einer Investmententscheidung diese Faktoren abgewogen und zusätzliche Risikohinweise berücksichtigt werden.
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Denn es kann keine außerordentlichen Chancen geben ohne Risiken auf der Gegenseite. Bitte bedenken Sie, dass es sich bei (vorbörslichen) Aktien nicht um Bundesschatzbriefe handelt, sondern um eine spekulative Anlage mit erheblichen Risiken. Diese können bis zu einem Totalverlust des Investments gehen.
                 </Typography>
-                <Typography variant="h6" gutterBottom  sx={{textDecoration: 'underline'}}>
+                <Typography variant="h6" gutterBottom  sx={{textDecoration: 'underline', fontSize: '1.5rem' }}>
                     Bitte lesen die folgenden Risikohinweise genau und überfliegen Sie diese nicht nur.
                 </Typography>
-                <Typography variant="body1" gutterBottom sx={{ fontWeight: 'bold', color: 'black', fontSize: '1.2rem' }}>
+                <Typography variant="body1" gutterBottom sx={{ fontWeight: 'bold', color: 'black', fontSize: '1.4rem' }}>
                     Zusätzlich zu dieser Zusammenfassung können Sie eine detaillierte Übersicht der Risiken, die auch Ihr Investment in Aktien der betreffen können, auf den Seiten 6-11 Ihres Zeichnungsscheines entnehmen. Prüfen Sie auch diesen genau, bevor Sie eine Unterschrift tätigen.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     1. Was sind Aktien?
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Aktien sind Beteiligungspapiere. Sie verkörpern die Beteiligung an dem Vermögen der Aktiengesellschaft und vermitteln, soweit die Stimmrechte nicht ausgeschlossen sind, auch Stimmrechte, die das Recht zur Beteiligung an der Aktionärsversammlung einschließen. Ein Ertrag wird erzielt, sofern eine Dividende (Gewinnbeteiligung) ausgezahlt wird und/oder zum Zeitpunkt des Verkaufs im Ergebnis ein Verkaufspreis erzielt wird, der über dem Anschaffungspreis liegt (Kurssteigerung).
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Eine Garantie für die Erzielung eines Ertrags mit der Kapitalanlage in Aktien kann nicht gegeben werden. Eine Kapitalanlage in Aktien ist immer eine Risikoanlage.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     2.Risiken des Unternehmens (unternehmerisches Risiko), Insolvenzrisiko
                 </Typography>
 
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Mit dem Erwerb der Aktie beteiligt sich der Anleger an der wirtschaftlichen Entwicklung der
                     Aktiengesellschaft, er wird Aktionär und damit Gesellschafter. Das unternehmerische Risiko beschreibt die
                     Gefahr, dass sich die Entwicklung des Unternehmens anders darstellt als erwartet. Ein wirtschaftlicher
@@ -160,10 +168,10 @@ const RiskAcceptanceModal = () => {
                     besonderen Situation des Unternehmens. Diese Faktoren beeinflussen den Wert und damit den
                     Wiederverkaufspreis der Aktie.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     3.Kursänderungsrisiko, Risiken der Rahmenbedingungen und der Marktpsychologie
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Aktienkurse weisen unvorhersehbare Schwankungen auf. Das allgemeine Marktrisiko einer Aktie ist das
                     Risiko einer Preisänderung, die der allgemeinen Tendenz am Aktienmarkt zuzuschreiben ist, und die in
                     keinem direkten Zusammenhang mit der wirtschaftlichen Situation des einzelnen Unternehmens steht. Dem
@@ -178,10 +186,10 @@ const RiskAcceptanceModal = () => {
                     können in der betriebswirtschaftlichen Situation der Gesellschaft liegen, können aber auch aus allgemeinen
                     volkswirtschaftlichen Faktoren resultieren.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     4.Länderrisiko und Risiken der Rechtewahrnehmung, Informationsrisiko
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Bei dem Erwerb ausländischen Aktien muss der Anleger beachten, dass er über den Verkaufspreis der Aktie
                     bei einer Veräußerung gegebenenfalls erst nach längeren Fristen oder nach einer Umschreibung verfügen
                     kann. Erwirbt der Anleger ausländische Aktien oder unterhält er sein Wertpapierdepot im Ausland, so kann
@@ -190,36 +198,36 @@ const RiskAcceptanceModal = () => {
                     transferieren. Dieses Risiko besteht besonders bei Ländern, in denen keine politisch stabilen Verhältnisse
                     gegeben sind.
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Will der Anleger darüber hinaus seine Rechte gegenüber der Gesellschaft wahrnehmen, bewegt er sich in
                     einem ausländischen Rechtskreis und möglicherweise in einer fremden Sprache. Unter Umständen muss er
                     ausländische Anwälte und Gerichte zu Hilfe nehmen. Dies ist mit zusätzlichen Kosten und Schwierigkeiten
                     verbunden.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     5.Währungsrisiko
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Ausländische Aktien repräsentieren häufig einen Wert in fremder Währung. Neben den der Aktie
                     immanenten Risiken trägt der Anleger zusätzlich das Währungsrisiko. Verluste können sich allein aus der
                     Tatsache ergeben oder vergrößern, dass sich die Aktienwährung gegenüber der heimischen Währung
                     verschlechtert.
                 </Typography>
 
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     6.Risiko eines Delisting
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Bei Aktien die an einer Börse oder einem anderen allgemein zugänglichen Sekundärmarkt
                     (Wiederverkaufsmarkt) gehandelt werden, besteht das Risiko einer Aussetzung des Handels bzw. sogar eines
                     Delistings. Während bei einer Aussetzung der Handel nur vorübergehende eingestellt wird, wird beim
                     Delisting die Aktiengesellschaft dauerhaft vom Handel entfernt, so dass die Aktien nicht mehr über die Börse
                     oder einem anderen allgemein zugänglichen Sekundärmarkt gehandelt werden können.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     7.Liquiditätsrisiko
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Unter der Liquidität einer Anlage wird die Möglichkeit des Verkaufes zu marktgerechten Preisen verstanden.
                     Von einer ausreichenden Liquidität wird ausgegangen, wenn ein Anleger seine Kapitalanlage jederzeit an
                     einem Markt verkaufen kann, ohne dass dies zu spürbaren Preisänderungen führt. Unter dem
@@ -230,10 +238,10 @@ const RiskAcceptanceModal = () => {
                     werden. Der Kreis der Interessenten ist oft so klein, dass der Verkauf solcher Aktien - wenn überhaupt - nur
                     unter schlechten Bedingungen möglich ist.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     8.Risiko bei einer kreditfinanzierten Kapitalanlage
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Bei Aktien handelt es sich um spekulative Kapitalanlagen. Das zum Erwerb eingesetzte Kapital sollte daher
                     nur aus freien Mitteln aufgebracht werden. Eine Fremdfinanzierung ist nicht zu verantworten. Die Aktien
                     stellen keine geeignete Sicherheitsleistung für eine Kreditaufnahme dar. Sollte der Anleger dennoch
@@ -245,28 +253,28 @@ const RiskAcceptanceModal = () => {
                     Fremdfinanzierung nicht aus anderen Einkommensquellen zurückgeführt werden kann, besteht das Risiko
                     einer Privatinsolvenz.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     9.Risiko der Transaktionskosten
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Transaktionen (u.a. Provisionen, Gebühren, Courtagen, Kapitalverkehrssteuern) verteuern den Ankaufspreis
                     und vermindern den Verkaufspreis. Sie schmälern damit die Gewinnchance und erhöhen umgekehrt das
                     Verlustrisiko.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     10.Dividendenrisiko
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Die auf eine Aktie gezahlte Dividende richtet sich stark nach den erzielten Gewinnen der Gesellschaft. In
                     ertragsschwachen Jahren kann nur eine geringe oder überhaupt keine Dividende gezahlt werden. Über die
                     Zahlung einer Dividende wird jedes Jahr neu entschieden, eine frühere Zahlung stellt daher keine Garantie
                     für die weitere Zahlung von Dividenden dar. Auch bei guter Entwicklung muss dies nicht eine erhöhte
                     Dividende zur Folge haben.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     11.Risiko der Nachzeichnung zum Schutz vor Vermögens- und Stimmrechtsverlusten
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Im Rahmen einer Kapitalerhöhung kann eine Aktiengesellschaft neue Aktien ausgeben. Den Aktionären steht
                     in diesem Fall in der Regel ein Bezugsrecht zu, sofern dieses nicht ausgeschlossen wurde. Durch die Ausgabe
                     neuer Aktien muss ein Altaktionär in Ausübung seines Bezugsrechts neue Aktien im Verhältnis seines
@@ -277,26 +285,26 @@ const RiskAcceptanceModal = () => {
                     entsprechende Anzahl an Bezugsrechten dazukaufen oder eine entsprechende Anzahl den neu
                     ausgegebenen Aktien zeichnen.
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Mit der Zeichnung von Aktien in Ausübung des Bezugsrechts, der Zeichnung neuer Aktien bzw. des Erwerbs
                     von Bezugsrechten ist ein erneuter Kapitalaufwand seitens des Anlegers verbunden. Erbringt der Altaktionär
                     dieses erbeuten Kapitalaufwand nicht, verringert sich sein Anteil an der Aktiengesellschaft und damit auch
                     das Verhältnis seines Gewinnanteils und seines Stimmrechts.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     12.Kursbestimmungsrisiko
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Der Aktienkurs von an der Börse oder einem anderen allgemein zugänglichen Sekundärmarkt
                     (Wiederverkaufsmarkt) gehandelten Aktien wird in der Regel mehrmals täglich bestimmt, so dass das Risiko
                     besteht, dass sich der Erwerbspreis zwischen der Auftragserteilung und der für den Erwerbspreis
                     maßgeblichen Ausführung für den Erwerber in erheblichem Ausmaß nachteilig verändern kann Selbiges gilt
                     auch im Fall der Veräußerung.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     13.Aktiensplit
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Dabei handelt es sich um eine Aktienteilung indem eine höhere Stückzahl der Aktien durch eine „Spaltung“
                     der Aktien erreicht wird. Bei einem Aktiensplit von 1 zu 10 erhält z.B. ein Aktionär, der 1 Aktie besitzt, zehn
                     weitere Aktien ohne Gegenleistung. Dies ist aber keine Ausgabe von Gratisaktien, da bei Nennwertaktien
@@ -304,32 +312,30 @@ const RiskAcceptanceModal = () => {
                     Nennwert dann nur noch € 5,-. Entsprechend verringert sich dann auch in der Regel der Kurs der einzelnen
                     Aktie.
                 </Typography>
-                <Typography variant="h6" gutterBottom>
+                <Typography variant="h6" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     14.Unvermeidbarkeit der vorgenannten Risiken
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Die vorgenannten Risiken bestehen in jedem Fall. Sie lassen sich nicht verringern oder vermeiden. Diese
                     Risiken lassen auch nicht ausschließen. Sofern Ihnen jemand gegenteilige Angaben macht, ist dies nicht
                     richtig.
                 </Typography>
-                <Typography variant="body1" gutterBottom>
+                <Typography variant="body1" gutterBottom sx={{ fontSize: '1.5rem' }}>
                     Diese Übersicht erhebt keinen Anspruch auf Vollständigkeit.
                 </Typography>
                 <Contacts/>
 
-
-
                 <FormControlLabel
                     control={<Checkbox checked={accepted} onChange={handleAcceptChange} />}
                     label="I agree to the terms and conditions"
-                    sx={{ mt: 2, }}
+                    sx={{ mt: 2, fontSize: '1.5rem' }}
                 />
                 <Button
                     variant="contained"
                     color="primary"
                     onClick={handleClose}
                     disabled={!accepted}
-                    sx={{ mt: 2 }}
+                    sx={{ mt: 2}}
                 >
                     Accept
                 </Button>
