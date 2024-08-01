f8b5267e72a7baa4b0258fe9efdaae7e3180d9a1
denisevseev <08038282@mail.ru>
2024-08-02 02:36:25 +0300

    changed text in RegulationAuthority.js

--- components/RegulationAuthority.js
+++ components/RegulationAuthority.js
@@ -6,13 +6,20 @@ const RegulationAuthority = () => {
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', padding: { xs: '2rem', sm: '5rem', md: '4rem', lg: '10rem' }, marginTop: isMobile && '2rem' }}>
-            <Paper elevation={3} sx={{ padding: { xs: '10px', sm: '20px' }, maxWidth: { xs: '100%', sm: '600px' }, width: '100%' }}>
+        <Box sx={{ width: isMobile ? '100%' : '80%', ml: isMobile ? 1 :  30, mt: 12,   textAlign: 'center' }}>
+            <Typography variant="h5" align="center" sx={{ fontWeight: 'bold', marginBottom: '2rem' }}>
+                Stabilität und Vertrauen: Die U.S. Securities and Exchange Commission (SEC) als Hüter der Finanzmärkte
+            </Typography>
+            <img src="https://smfanton.ru/wp-content/uploads/2019/07/shapka.jpg" alt="U.S. Securities and Exchange Commission" style={{ width: isMobile ? '100%' : '60%', height: 'auto' }} />
+            <Typography elevation={3} sx={{ padding: { xs: '10px', sm: '20px', textAlign: 'left' }, maxWidth: '100%' }}>
                 <Typography variant="body1" paragraph sx={{ fontSize: isMobile ? '22px' : '21px' }}>
                     Die US-amerikanische Börsenaufsicht SEC (Securities and Exchange Commission) beaufsichtigt die Finanzmärkte in den USA und ist eine der ältesten Organisationen zur Überwachung der Aktivitäten von Finanzdienstleistern, Banken und Unternehmen.
                 </Typography>
                 <Typography variant="body1" paragraph sx={{ fontSize: isMobile ? '22px' : '21px' }}>
                     Ihr Hauptziel ist es, ein funktionsfähiges, zuverlässiges und stabiles Finanzsystem zu gewährleisten und Bankkunden, Versicherte und Anleger zu schützen.
+                </Typography>
+                <Typography variant="body1" paragraph sx={{ fontSize: isMobile ? '22px' : '21px' }}>
+                    Die SEC (Securities and Exchange Commission) ist eine der wichtigsten staatlichen Aufsichtsbehörden in den USA. Sie ist für die Regulierung des Wertpapiermarktes verantwortlich, einschließlich des Handels mit Aktien, ihrer Emission, der Börsen, Makler, der Handelsoperationen von Privatpersonen und Investmentgesellschaften. Die SEC überwacht auch Fusionen und Übernahmen von Unternehmen. Dies bedeutet: Wenn ein Vermögenswert an den US-Börsen gehandelt wird oder einem amerikanischen Makler zugänglich ist, unterliegt er automatisch den von der SEC festgelegten Regeln. Das Hauptziel dieser Organisation besteht darin, Transparenz in Transaktionen zu gewährleisten und betrügerische Aktivitäten zu bekämpfen.
                 </Typography>
                 <Box mt={2}>
                     <Typography variant="body1" paragraph sx={{ fontSize: isMobile ? '22px' : '21px' }}>
@@ -27,7 +34,7 @@ const RegulationAuthority = () => {
                         Link zu Victorum Capital Inc. bei der SEC: <Link href="https://www.sec.gov/Archives/edgar/data/0001871600/000187160021000001/xslFormDX01/primary_doc.xml" target="_blank" rel="noopener">SEC Listing</Link>
                     </Typography>
                 </Box>
-            </Paper>
+            </Typography>
         </Box>
     );
 };
