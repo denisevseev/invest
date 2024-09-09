e718751675154c277064d3663babcae85c768b91
denisevseev <08038282@mail.ru>
2024-09-09 19:18:03 +0300

    still change lang de and en

--- components/ContactDetails.js
+++ components/ContactDetails.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { Grid, Typography, Box, Link, useTheme, useMediaQuery } from '@mui/material';
+import store from './../stores/userStore';
+import en from './../public/lang/en.json';
+import de from './../public/lang/de.json';
 
 const ContactDetails = () => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-    const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
 
-    // Общие отступы и стили для заголовков
+    // Определение текущего языка
+    const language = store.lang === 'de' ? de : en;
+
+    // Стили для заголовков
     const headerStyles = {
         fontWeight: 'bold',
         color: 'black',
         marginBottom: 2,
         marginTop: 2,
+        fontFamily: 'DejaVu Sans, sans-serif', // Шрифт для заголовков
     };
 
+    // Стили для основного текста
     const sectionStyles = {
         marginBottom: 2,
+        fontFamily: 'DejaVu Sans, sans-serif', // Шрифт для текста
+    };
+
+    // Стили для ссылок
+    const linkStyles = {
+        color: 'primary.main',
+        marginBottom: 2,
+        fontFamily: 'DejaVu Sans, sans-serif', // Шрифт для ссылок
     };
 
     return (
@@ -43,8 +58,8 @@ const ContactDetails = () => {
                 },
             }}
         >
-            <Typography variant="h4" sx={{ marginBottom: 3, textAlign: 'center', fontWeight: 'bold', color: 'black' }}>
-                Contact Information
+            <Typography variant="h4" sx={{ marginBottom: 3, textAlign: 'center', fontWeight: 'bold', color: 'black', fontFamily: 'DejaVu Sans, sans-serif' }}>
+                {language.contactDetails.title}
             </Typography>
             <Box
                 component="img"
@@ -63,68 +78,68 @@ const ContactDetails = () => {
                 {/* Блок компании */}
                 <Grid item xs={12} sm={6} md={4}>
                     <Typography variant="body1" sx={headerStyles}>
-                        COMPANY:
+                        {language.contactDetails.companyHeader}
                     </Typography>
-                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>Victorum Capital Inc.</Typography>
-                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>2102-58 Keefer Place</Typography>
-                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>Vancouver, BC V6B 0B6</Typography>
-                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>Canada</Typography>
-                    <Typography variant="body1" sx={{ fontWeight: 'bold', color: 'black', ...sectionStyles }}>CEO: Osbert Doehl</Typography>
+                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>{language.contactDetails.companyName}</Typography>
+                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>{language.contactDetails.address.line1}</Typography>
+                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>{language.contactDetails.address.line2}</Typography>
+                    <Typography variant="body1" sx={{ color: 'black', ...sectionStyles }}>{language.contactDetails.address.country}</Typography>
+                    <Typography variant="body1" sx={{ fontWeight: 'bold', color: 'black', ...sectionStyles }}>{language.contactDetails.ceo}</Typography>
                 </Grid>
                 {/* Блок контактов */}
                 <Grid item xs={12} sm={6} md={4}>
                     <Typography variant="body1" sx={headerStyles}>
-                        CONTACTS:
+                        {language.contactDetails.contactsHeader}
                     </Typography>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
-                        Main Telephone:
+                        {language.contactDetails.mainTelephone}
                     </Typography>
-                    <Link href="tel:+16042600738" sx={{ color: 'primary.main', ...sectionStyles }}>
+                    <Link href="tel:+16042600738" sx={linkStyles}>
                         +1 604-260-0738
                     </Link>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
-                        Main Email:
+                        {language.contactDetails.mainEmail}
                     </Typography>
-                    <Link href="mailto:support@victorum-capital.com" sx={{ color: 'primary.main', ...sectionStyles }}>
+                    <Link href="mailto:support@victorum-capital.com" sx={linkStyles}>
                         support@victorum-capital.com
                     </Link>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
-                        Compliance:
+                        {language.contactDetails.compliance}
                     </Typography>
-                    <Link href="mailto:compliance@victorum-capital.com" sx={{ color: 'primary.main', ...sectionStyles }}>
+                    <Link href="mailto:compliance@victorum-capital.com" sx={linkStyles}>
                         compliance@victorum-capital.com
                     </Link>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
-                        Media:
+                        {language.contactDetails.media}
                     </Typography>
-                    <Link href="mailto:press@victorum-capital.com" sx={{ color: 'primary.main', ...sectionStyles }}>
+                    <Link href="mailto:press@victorum-capital.com" sx={linkStyles}>
                         press@victorum-capital.com
                     </Link>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
-                        Administration:
+                        {language.contactDetails.administration}
                     </Typography>
-                    <Link href="mailto:administration@victorum-capital.com" sx={{ color: 'primary.main', ...sectionStyles }}>
+                    <Link href="mailto:administration@victorum-capital.com" sx={linkStyles}>
                         administration@victorum-capital.com
                     </Link>
                 </Grid>
                 {/* Блок регистрационных данных */}
                 <Grid item xs={12} sm={6} md={4}>
                     <Typography variant="body1" sx={headerStyles}>
-                        REGISTRATION:
+                        {language.contactDetails.registrationHeader}
                     </Typography>
-                    <Typography variant="body1" sx={{ ...sectionStyles }}>State: British Columbia (BC)</Typography>
-                    <Typography variant="body1" sx={{ ...sectionStyles }}>Registration number: 790478002BC0001</Typography>
+                    <Typography variant="body1" sx={{ ...sectionStyles }}>{language.contactDetails.state}</Typography>
+                    <Typography variant="body1" sx={{ ...sectionStyles }}>{language.contactDetails.registrationNumber}</Typography>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
-                        Share Style: Common CLASS A RESTRICTED SHARES
+                        {language.contactDetails.shareStyle}
                     </Typography>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
                         ISIN:
                     </Typography>
-                    <Typography variant="body1" sx={{ ...sectionStyles }}>CA92642D1024</Typography>
+                    <Typography variant="body1" sx={{ ...sectionStyles }}>{language.contactDetails.isin}</Typography>
                     <Typography variant="body1" sx={{ fontWeight: 'bold', ...sectionStyles }}>
                         CUSIP:
                     </Typography>
-                    <Typography variant="body1" sx={{ ...sectionStyles }}>92642D102</Typography>
+                    <Typography variant="body1" sx={{ ...sectionStyles }}>{language.contactDetails.cusip}</Typography>
                 </Grid>
             </Grid>
         </Box>
